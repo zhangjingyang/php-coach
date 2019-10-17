a3b3c50f67d13363b1f7e3d96474c27de9f2ee32
Thomas Young <450724834@qq.com>
2019-10-17 10:36:41 +0800

    123123

--- app/Exceptions/ValidateException.php
+++ app/Exceptions/ValidateException.php
@@ -8,11 +8,7 @@ class ValidateException extends Exception
 {
     public function render()
     {
-<<<<<<< HEAD
         $_SESSION['ERROR_MESSAGE'] = $this->getMessage();
-=======
-        $_SESSION['ERROR_MESSAGE'] = '登录失败';
->>>>>>> 57c5adbbc200f92b13cb2ac12530ea9ed2b40d83
         header('Location:index.php');
     }
 }
--- app/Services/Login.php
+++ app/Services/Login.php
@@ -13,13 +13,7 @@ class Login
             throw new ValidateException("用户名不能为空");
         }
         $pdo = DB::getInstance();
-<<<<<<< HEAD
         $result = $pdo->table('users')->where("name = {$_POST['username']}")->get();
-=======
-        $result = $pdo->table('users')->get();
-        var_dump($result);
->>>>>>> 57c5adbbc200f92b13cb2ac12530ea9ed2b40d83
-
         $_SESSION['VALIDATE_ERROR_MESSAGE'] = "";
         View::make('success');
     }
